53d4589e2cbf3dadc4ce955a1dc70c2c05080171
Karen_Camacho <karencamacho484@gmail.com>
2024-12-09 13:24:53 -0500

    Implementacion de android studio

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const app = express();
 const loginRoute = require('./routes/login'); 
 const productsRoute = require('./routes/products'); 
 
-const corsOptions = {
-    origin: ['http://localhost', 'http://localhost:8100', 'https://proyectoionic.onrender.com'], // Orígenes permitidos
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-    credentials: true,
-  };
-  
-  app.use(cors(corsOptions));
+app.use(cors());
 
 // Middleware para manejar JSON
 app.use(express.json());
